da636f266966c2a8003174aa99e8fd5c811e4b60
Mélissa Begyn <mel.begyn@live.fr>
2020-06-07 18:09:58 -0700

    fixing sql server config + clean code

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
-
+import { Component, Inject, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+  Concerts: Concert[] = []
+
+  constructor(http: HttpClient, @Inject('BASE URL') baseUrl: string) {
+    http.get<Concert[]>(baseUrl + 'api/Concerts').subscribe(result => {
+      this.Concerts = result;
+    }, error => console.error(error))
+  }
+}
+
+
+interface Concert {
+  id: number;
+  TourName: string;
+  Artist: string;
+  Stage: string;
+  ConcertDate: Date;
+  NumberTicketsAvailable: number;
+  TicketPrice: number;
 }
